d2a1dc6b0192869406aef21467e3c595cd299e0a
Ritesh <ritesh@Riteshs-MacBook-Pro.local>
2024-07-30 22:00:27 +0530

    Published the first version.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import packageJson from "./package.json" assert { type: "json" };
+import * as fs from 'fs';
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 export default [
   {
