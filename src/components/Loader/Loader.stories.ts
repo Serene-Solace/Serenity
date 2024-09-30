8b23f63b0351c381a9befd1119792a00e5a7b17a
bhupeshyadav607 <bhupeshyadav671@gmail.com>
2024-09-30 10:05:23 +0530

    loader, menuItem & modal

---
+++ src/components/Loader/Loader.stories.ts
@@ -0,0 +1,16 @@
+import {Meta, StoryObj} from '@storybook/react'
+import Loader from './Loader'
+
+const meta = {
+  title: "UI/Loader",
+  component: Loader,
+  tags: ["autodocs"],
+  parameters: { layout: "centered" },
+} satisfies Meta<typeof Loader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const CustomLoader: Story = {
+    args: {isLoading: true}
+}
